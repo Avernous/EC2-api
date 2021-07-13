c8dd992a0979017cfc89fa8ff9a0a0159a21c354
Avernous <fouksconnor@gmail.com>
2021-07-13 17:43:23 -0500

    swagger documentation server initial setup

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 const express = require('express')
 const app = express();
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 
 const port = 3000;
+// options for generating documentation
+const swaggerOptions = {
+    swaggerDefinition:{
+        info: {
+            title: "EC2 - API",
+            description: "data resource for Eclipse Phase 2",
+            contact: {
+                name: "Connor Fouks"
+            },
+            servers: ["http://localhost:3000"],
+        }
+    },
+    apis: ["app.js"]
+}
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 //Routes
 app.get('/customers', (req, res) => {
