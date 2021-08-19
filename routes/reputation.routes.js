88eff8060e53f3dc52cb806e82c55a19cccc3adb
Avernous <fouksconnor@gmail.com>
2021-08-19 13:23:41 -0500

    add reputation routes

---
+++ routes/reputation.routes.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const router = express.Router();
+const Reputation = require('../models/reputation');
+// url: /api/v1/pools
+
+ /**
+  * @swagger
+  *     /api/v1/pools:
+  *     get:
+  *         description: Used to request all pools
+  *         responses:
+  *             '200':
+  *                 description: A successful respone
+  */
+router.get('/reputations', (req, res) => {
+    Reputation.find((err, results) => {
+        if(err){
+            console.log(err.name);
+            res.sendStatus(500);
+        }
+        else{
+            res.json(results)
+        }
+    })
+});
+
+ /**
+  * @swagger
+  *     /api/v1/pools/:id:
+  *     get:
+  *         description: Used to request all pools
+  *         responses:
+  *             '200':
+  *                 description: A successful respone
+  */
+router.get('/reputations/:id', (req, res) => {
+    console.log(req.params.id);
+    Reputation.findById(req.params.id, (err, results) => {
+        if(err){
+            if(err.name === "CastError"){
+                res.sendStatus(400);
+            }
+            else{
+                res.sendStatus(500);
+            } 
+        }
+        else{
+            res.json(results);
+        }
+    })
+
+});
+
+
+module.exports = router;
--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const poolRouter = require('./pool.routes.js')
+const reputationRouter = require('./reputation.routes')
 // url: /api/v1
 
 
 router.use('/', poolRouter);
+router.use('/', reputationRouter);
 
 
 module.exports = router;
